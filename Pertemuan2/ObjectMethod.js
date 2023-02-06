40293f84f168af7a20df5d70c51e2c9105a78578
laoderidho <2081007@unai.edu>
2023-02-06 10:49:27 +0700

    update lagi

--- Pertemuan2/ObjectMethod.js
+++ Pertemuan2/ObjectMethod.js
@@ -4,6 +4,9 @@ const Person = {
     //di bawah ini adalah method yang hampir sama dengan function tapi dalam bentuk object
     greet(myName , age){
         console.log(`Hello I ${myName} and i ${age} years old`)
+    },
+    Sum(num1, num2){
+        console.log(num1+num2)
     }
 }
 
@@ -12,3 +15,4 @@ const age = 19;
 
 //seperti inilah cara memanggil Method
 Person.greet(nama, age)
+Person.Sum(2,3)
